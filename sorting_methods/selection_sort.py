558261b2d2148dddd4fcf6c801dadbd5bb2830b1
Aman Raj <aman.raj@metlife.com>
2023-03-06 13:00:21 +0530

    selection sort updated

--- sorting_methods/selection_sort.py
+++ sorting_methods/selection_sort.py
@@ -2,9 +2,10 @@
 lst = [4,3,8,1,9,2]
 
 for i in range(len(lst)-1):
-
+    min_idx = i
     for j in range(i+1, len(lst)):
-        if lst[i] > lst[j]:
-            lst[j], lst[i] = lst[i], lst[j]
+        if lst[min_idx] > lst[j]:
+            min_idx = j
+    lst[min_idx], lst[i] = lst[i], lst[min_idx]
 
 print(lst)
